1212d47125ba915a9c84ce9c99c1db5af50fbaf7
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-31 13:34:27 +0000

    Update combine_all_videos.py
    Currently, this compresses a lot of the quality when compiling all the videos into one video, so be wary of using this.

--- main/combine_all_videos.py
+++ main/combine_all_videos.py
@@ -1,8 +1,6 @@
 from moviepy.editor import *
 import os
-
-path="/home/user/testfolder"
-
+path = "/home/user/Downloads"
 def combine_videos(folder_path):
     # Get the list of all files in the folder
     all_files = os.listdir(folder_path)
@@ -11,7 +9,10 @@ def combine_videos(folder_path):
     videos = [f for f in all_files if f.endswith(".mp4") or f.endswith(".avi") or f.endswith(".mkv")]
 
     # Combine the videos
-    final_video = concatenate_videoclips([VideoFileClip(os.path.join(folder_path, v)) for v in videos])
+    final_video = concatenate_videoclips([
+        VideoFileClip(os.path.join(folder_path, v)).resize(height=360).set_pos("center")
+        for v in videos
+    ], method="compose")
 
     # Write the final video to disk
     final_video.write_videofile("final_video.mp4")
